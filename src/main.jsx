7cdf3f6feee52ffcc225808c7c5021b03069f8cb
Tamiris Cristine <tamiris.css@gmail.com>
2025-04-11 15:08:12 +0200

    feat: add react router setup with initial pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import HomePage from './pages/home'
+import LoginPage from './pages/login'
+import SignupPage from './pages/signup'
+import NotFoundPage from './pages/not-found'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 )
