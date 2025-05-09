b6b655abd546f099545f94574c965550ca3abd76
Tamiris Cristine <tamiris.css@gmail.com>
2025-05-09 14:38:20 +0200

    feat: add loading to signup button

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,3 +1,4 @@
+import { Loader2Icon } from 'lucide-react'
 import { Link, Navigate } from 'react-router'
 
 import { Button } from '@/components/ui/button'
@@ -152,7 +153,12 @@ const SignupPage = () => {
               />
             </CardContent>
             <CardFooter>
-              <Button className="w-full">Criar conta</Button>
+              <Button className="w-full" disabled={form.formState.isSubmitting}>
+                {form.formState.isSubmitting && (
+                  <Loader2Icon className="animate-spin" />
+                )}
+                Criar conta
+              </Button>
             </CardFooter>
           </Card>
         </form>
